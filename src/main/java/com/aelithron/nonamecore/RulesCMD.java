97a138406692f01092cf2b7c1c3fbb601c4f00fb
aelithron <aelithron@gmail.com>
2024-12-06 00:23:17 -0700

    Add the rule book This feature creates a book of rules, which any player can open with /rules. The original behavior (flooding the rules into chat) is still available for non-player senders.
    Signed-off-by: aelithron <aelithron@gmail.com>

--- src/main/java/com/aelithron/nonamecore/RulesCMD.java
+++ src/main/java/com/aelithron/nonamecore/RulesCMD.java
@@ -1,9 +1,13 @@
 package com.aelithron.nonamecore;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.BookMeta;
 
 public class RulesCMD implements CommandExecutor {
     private NoNameCore plugin;
@@ -16,11 +20,31 @@ public class RulesCMD implements CommandExecutor {
         String prefix = CoreTools.getInstance().getPrefix();
         sender.sendMessage(prefix + ChatColor.DARK_GRAY + "-- " + ChatColor.AQUA + "Server Rules" + ChatColor.DARK_GRAY + " --");
         int i = 1;
-        for (String rule : plugin.getConfig().getStringList("Rules")) {
-            sender.sendMessage(ChatColor.AQUA.toString() + i + ". " + ChatColor.translateAlternateColorCodes('&', rule));
-            i++;
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            // Book mode setup things
+            ItemStack ruleBook = new ItemStack(Material.WRITABLE_BOOK);
+            BookMeta ruleMeta = (BookMeta) ruleBook.getItemMeta();
+            ruleMeta.setGeneration(BookMeta.Generation.ORIGINAL);
+            ruleMeta.setAuthor("Server Managers");
+            ruleMeta.setTitle("Server Rules");
+            ruleMeta.addPage(
+                    ChatColor.DARK_GRAY + "---- " + ChatColor.AQUA + "Server Rules" + ChatColor.DARK_GRAY + " ----",
+                    ChatColor.GRAY + "One rule per page of the book.",
+                    ChatColor.LIGHT_PURPLE + "Please note, these can change at any time.");
+            for (String rule : plugin.getConfig().getStringList("Rules")) {
+                ruleMeta.addPage(ChatColor.AQUA.toString() + i + ". " + ChatColor.translateAlternateColorCodes('&', rule));
+                i++;
+            }
+            ruleBook.setItemMeta(ruleMeta);
+            player.openBook(ruleBook);
+        } else {
+            for (String rule : plugin.getConfig().getStringList("Rules")) {
+                sender.sendMessage(ChatColor.AQUA.toString() + i + ". " + ChatColor.translateAlternateColorCodes('&', rule));
+                i++;
+            }
+            sender.sendMessage(ChatColor.LIGHT_PURPLE + "Please note, these can change at any time.");
         }
-        sender.sendMessage(ChatColor.LIGHT_PURPLE + "Please note, these can change at any time.");
-        return false;
+        return true;
     }
 }
